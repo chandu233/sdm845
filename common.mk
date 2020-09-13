321d11747a39856ec9094dae829c6a301ae08d9a
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-13 06:04:58 +0000

    sdm845-common: build libhwbinder and copy some prebuilts from vndk29
    Test: build, ril
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>

--- common.mk
+++ common.mk
@@ -92,6 +92,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor \
+    libhidltransport
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # Camera HIDL
 PRODUCT_PACKAGES += \
     vendor.oneplus.camera.CameraHIDL@1.0 \
